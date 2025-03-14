a540fa7ef707dfb734f4941ebbfb8d4c6de1b40b
Shuban Paranjape <75947480+BACKACHIKA@users.noreply.github.com>
2025-03-14 22:39:42 +0000

    Update TodoList.py

--- TodoList.py
+++ TodoList.py
@@ -39,8 +39,7 @@ with tab1:
                 json.dump(userdata, file)
              
 
-    usabledata=json.loads(userdata)
-    for i in range(len(usabledata["tasks"])):
+    for i in range(len(userdata["tasks"])):
         col1, col2, col3 = st.columns([2, 1, 1])
         with col1:
             st.write(usabledata["tasks"][i])
