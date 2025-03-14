a1785ceb119aea10e7007af5be7a7cca7a269221
Shuban Paranjape <75947480+BACKACHIKA@users.noreply.github.com>
2025-03-14 23:05:21 +0000

    Update TodoList.py

--- TodoList.py
+++ TodoList.py
@@ -49,7 +49,7 @@ with tab1:
             
 
         response = model.generate_content(
-            f'Break down the following task: {userdata][i]} into chunks that can be completed in. sessions. '
+            f'Break down the following task: {userdata[i]} into chunks that can be completed in. sessions. '
             'Split it into stages, so Stage 1: Do this, Stage 2: Do this, and so on for 10 stages. '
             'Each stage must have max. 20 words. Keep it all the same font. Add a new line before every stage. '
             'However if a task is given,you must break it down.Like you need to.Even if its a repeat task,you need to.No matter what break down the task.Don\'t repeat the prompt in your response ever.'
