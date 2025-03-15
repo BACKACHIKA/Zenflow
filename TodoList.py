a217cff2d4a2d4eb092c12bb425341372ded896f
Shuban Paranjape <75947480+BACKACHIKA@users.noreply.github.com>
2025-03-15 09:50:05 +0000

    Update TodoList.py

--- TodoList.py
+++ TodoList.py
@@ -1,4 +1,4 @@
-import streamlit as st
+importance streamlit as st
 import json
 import os
 import time
@@ -40,7 +40,7 @@ with tab1:
                 userdata["tasks"].append({"task": task, "deadline": str(deadline)})
                 with open(user, "w") as file:
                     json.dump(userdata, file)
-                st.rerun()
+                
 
     for i in range(len(userdata["tasks"])):
         task_data = userdata["tasks"][i]
